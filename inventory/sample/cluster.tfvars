4eafc84a258a88344e39d40415e5eda794620151
eroan-marie <marieeroan@gmail.com>
2024-01-08 16:28:03 +0100

    Feat: add elb, secgroup and natgateway

--- inventory/sample/cluster.tfvars
+++ inventory/sample/cluster.tfvars
@@ -1,11 +1,11 @@
-image_name = "OBS Ubuntu 20.04"
+image_name = "OBS Ubuntu 22.04"
 vpc_cidr = "192.168.0.0/16"
 vpc_subnet_cidr = "192.168.0.0/17"
 
 cluster_configuration = {
     master = {
         flavor = "s6.small.1"
-        amount = 2
+        amount = 1
         additionnal_disk_size = 0
         type = "master"
     }
@@ -25,3 +25,5 @@ cluster_configuration = {
 
 cluster_name = "rs-cluster"
 buckets = ["elasticsearch-processing", "elasticsearch-security", "thanos", "loki"]
+
+public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPm/faFdHGZ1smWQ/yBFAB/YlsYRdpKj9OfPCwFAyc5vhg4SLGBDhgIdwOeDMxpffOwxdapH1Zaph4mponQx+b9kqJ7NdCvh3u7EkarVmnEjbTd9dzZMQ+Xbz6WZEF6zRXaqSe5SgE7NsqgGalo2wT9NDUFXqIzX+eV4z9b2xUz+c0ZDCw9t0XQc2QQSlbxjWnyPcTiyk9Nm59a8wHXWWTerUdh821HVRlfYucwW1q17WSvuiH+bJh1pyOj7RzEhZ/3tPHE1BTiMerXO5ZnPk/VB/IIjWsmpDIBKoWi6+bXpoHjPpWtbNLg9lfOxSgNOaCMvwV0FtrPiqbru2uR5Gd"
--- modules/create-cluster/main.tf
+++ modules/create-cluster/main.tf
@@ -1,17 +1,6 @@
 terraform {
 required_version = ">= 1.6.0"
 
-backend "s3" {
-  bucket   = "terraformbucket"
-  key      = "terraform.tfstate"
-  region   = "eu-west-0"
-  endpoint = "https://oss.eu-west-0.prod-cloud-ocb.orange-business.com"
-  
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-}
-
 required_providers {
   flexibleengine    = {
     source          = "FlexibleEngineCloud/flexibleengine"
@@ -29,33 +18,32 @@ variable "image_name" {
 variable "cluster_configuration" {
     description = "infrastructure configuration"
     type        = map(object({
-      flavor    = string
-      amount    = number
-      type      = string
+      flavor                = string
+      amount                = number
+      type                  = string
       additionnal_disk_size = number
     }))
 }
-
-
 
 resource "flexibleengine_compute_instance_v2" "nodes" {
   for_each = {
     for item in flatten([
       for nodes in var.cluster_configuration : [
         for count in range(nodes.amount) : {
-          name  = nodes.type
-          flavor = nodes.flavor
-          number = count
+          name                  = nodes.type
+          flavor                = nodes.flavor
+          number                = count
           additionnal_disk_size = nodes.additionnal_disk_size
         }
       ]
     ])
-    : "${item.name}.${item.flavor}.${item.number}" => item
+    : "${item.name}.${item.number}" => item
   }
   name            = "${each.value.name}-${each.value.number}"
   image_name      = var.image_name
   flavor_id       = each.value.flavor
-  security_groups = [var.secgroup.name]
+  key_pair        = "${var.cluster_name}-keypair"
+  security_groups = [flexibleengine_networking_secgroup_v2.secgroup.name]
   network {
     uuid = flexibleengine_vpc_subnet_v1.vpc_subnet.id
   }
@@ -72,18 +60,25 @@ resource "flexibleengine_compute_instance_v2" "nodes" {
   tags = {
     type  = each.value.name
   }
+  depends_on = [
+    flexibleengine_compute_keypair_v2.keypair
+  ]
 }
 
+resource "flexibleengine_compute_keypair_v2" "keypair" {
+  name       = "${var.cluster_name}-keypair"
+  public_key = var.public_key
+}
 
 #Network configuration
 variable "vpc_cidr" {
-    description = "network cidr"
-    type        = string
+  description = "network cidr"
+  type        = string
 }
 
 variable "vpc_subnet_cidr" {
-    description = "subnet cidr"
-    type        = string
+  description = "subnet cidr"
+  type        = string
 }
 
 resource "flexibleengine_vpc_v1" "main_vpc" {
@@ -99,18 +94,34 @@ resource "flexibleengine_vpc_subnet_v1" "vpc_subnet" {
 }
 
 resource "flexibleengine_nat_gateway_v2" "nat_gateway" {
-  name        = "nat_test"
-  description = "test for terraform"
-  spec        = "1"
-  vpc_id      = flexibleengine_vpc_v1.main_vpc.id
-  subnet_id   = flexibleengine_vpc_subnet_v1.vpc_subnet.id
+  name      = "nat_test"
+  spec      = "1"
+  vpc_id    = flexibleengine_vpc_v1.main_vpc.id
+  subnet_id = flexibleengine_vpc_subnet_v1.vpc_subnet.id
+}
+
+resource "flexibleengine_nat_snat_rule_v2" "snat_rule" {
+  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_gateway.id
+  floating_ip_id = flexibleengine_vpc_eip.eip_nat_gw.id
+  subnet_id      = flexibleengine_vpc_subnet_v1.vpc_subnet.id
+}
+
+resource "flexibleengine_vpc_eip" "eip_nat_gw" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name       = "bandwidth_nat_gw"
+    size       = 10
+    share_type = "PER"
+  }
 }
 
 resource "flexibleengine_networking_secgroup_v2" "secgroup" {
-  name        = "secgroup"
+  name = "secgroup"
 }
 
-resource "flexibleengine_networking_secgroup_rule_v2" "secgroup_rule_1" {
+resource "flexibleengine_networking_secgroup_rule_v2" "allow_ssh_ingress" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -129,9 +140,47 @@ resource "flexibleengine_lb_loadbalancer_v3" "elb" {
 
   availability_zone = [
     "eu-west-0a",
+    "eu-west-0b"
   ]
+  ipv4_eip_id = flexibleengine_vpc_eip.elb_eip.id
 
-  bandwidth_charge_mode = "traffic"
-  sharetype             = "PER"
-  bandwidth_size        = 10
+  backend_subnets   = [
+    flexibleengine_vpc_subnet_v1.vpc_subnet.id
+  ]
+}
+
+resource "flexibleengine_vpc_eip" "elb_eip" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name       = "bandwidth_elb"
+    size       = 10
+    share_type = "PER"
+  }
+}
+
+resource "flexibleengine_lb_listener_v3" "listener" {
+  name            = "elb-listener"
+  description     = "listener"
+  protocol        = "TCP"
+  protocol_port   = 22
+  loadbalancer_id = flexibleengine_lb_loadbalancer_v3.elb.id
+}
+
+resource "flexibleengine_lb_pool_v3" "pool" {
+  protocol    = "TCP"
+  lb_method   = "SOURCE_IP"
+  listener_id = flexibleengine_lb_listener_v3.listener.id
+}
+
+resource "flexibleengine_lb_member_v3" "member" {
+  address       = flexibleengine_compute_instance_v2.nodes["master.0"].access_ip_v4
+  protocol_port = 22
+  pool_id       = flexibleengine_lb_pool_v3.pool.id
+  subnet_id     = flexibleengine_vpc_subnet_v1.vpc_subnet.ipv4_subnet_id
+}
+
+output "eip_addr" {
+  value = flexibleengine_vpc_eip.elb_eip.address
 }
