f4770ed9f0bd327459d07051233b6ae921c2107a
eroan-marie <marieeroan@gmail.com>
2025-02-11 14:29:23 +0100

    cherry-pick 162bd77

---
+++ roles/terraform/cluster/tasks/variables.tf
@@ -0,0 +1,61 @@
+# Variables part
+
+variable "cluster_name" {
+    description = "cluster name"
+    type        = string
+}
+
+variable "region" {
+    description = "OVH region"
+    type        = string
+}
+
+variable "allowed_ip_list" {
+    description = "list of allowed ip"
+    type        = string
+}
+
+variable "nodepool_infra_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "nodepool_processing_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "nodepool_processing_access_csc_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "nodepool_processing_prefect_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "nodepool_processing_ondemand_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "nodepool_processing_systematic_desired_nodes" {
+    description = "number of desired nodes on nodepool"
+    type        = number 
+}
+
+variable "buckets" {
+    description = "bucket list"
+    type        = list(string)
+}
+
+variable "buckets_region" {
+    description = "buckets region"
+    type        = string
+}
+
+variable "public_key" {
+    description = "public_key"
+    type = string
+}
