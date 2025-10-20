ff590d449809ccc30af2153441f492aab59e0dc2
Vincent Privat <vincent.privat@airbus.com>
2025-10-20 17:21:13 +0200

    Externalize install-requirements.sh
    To avoid copy-paste in infra-monitoring / workflow-env

---
+++ .github/common/resources/install-requirements.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Copyright 2025 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+# Install miniforge
+mkdir -p ~/miniforge3
+wget -q "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" -O ~/miniforge3/miniforge.sh
+bash ~/miniforge3/miniforge.sh -b -u -p ~/miniforge3
+rm -f ~/miniforge3/miniforge.sh
+
+# Init conda
+~/miniforge3/bin/conda init bash
+conda update -n base -c defaults conda
+
+# Create conda env with python
+# conda update -n base -c defaults conda
+conda create -y -n rspy python=3.11
+
+# Install Ansible, Terraform, Openstackclient
+conda run -n rspy conda install -y -c conda-forge ansible terraform python-openstackclient passlib boto3 kubernetes-helm kubernetes-client python-kubernetes
+
+conda run -n rspy ansible-galaxy collection install openstack.cloud amazon.aws kubernetes.core community.general
+
+ln -s /usr/share/miniconda/envs/rspy/bin/kubectl /usr/local/bin/kubectl
