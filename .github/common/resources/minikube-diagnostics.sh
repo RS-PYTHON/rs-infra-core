c2a9b5c3c904007a9299053f32a440a2df03507b
nleconte-csgroup <86782407+nleconte-csgroup@users.noreply.github.com>
2026-02-24 15:28:49 +0100

    feat: add cluster roles / bindings for k8s diagnostics

--- .github/common/resources/minikube-diagnostics.sh
+++ .github/common/resources/minikube-diagnostics.sh
@@ -61,6 +61,14 @@ echo "=== 🪪 Service Accounts ==="
 kubectl get sa -A
 
 echo ""
+echo "=== 🪪 Cluster Roles ==="
+kubectl get cr -A
+
+echo ""
+echo "=== 🪪 Cluster Role Bindings ==="
+kubectl get crb -A
+
+echo ""
 echo "=== 🧩 Custom Resource Definitions ==="
 kubectl get crd -A
 
