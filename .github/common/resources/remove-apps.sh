22f4683ed1e6984e1dad5f34ad10e8033b7d2188
nleconte-csgroup <nicolas.leconte@cs-soprasteria.com>
2026-07-29 15:25:25 +0200

    ci(debug): assign static pool in metallb

--- .github/common/resources/remove-apps.sh
+++ .github/common/resources/remove-apps.sh
@@ -20,5 +20,4 @@ APPS="${APPS_DIR:-apps}"
 # Remove specific apps for OVH that can't run on the CI
 rm -rf \
   "${APPS}/01-csi-driver-nfs" \
-  "${APPS}/01-openstack-manila-csi" \
-  #"${APPS}/03-envoy-gateway"
+  "${APPS}/01-openstack-manila-csi"
