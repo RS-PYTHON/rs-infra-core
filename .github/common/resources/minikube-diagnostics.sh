dd72028a1b3b0ff13b2acde5e8d972cb2e00dc2d
nleconte-csgroup <nicolas.leconte@cs-soprasteria.com>
2026-08-05 15:27:49 +0200

    ci: debug ci

--- .github/common/resources/minikube-diagnostics.sh
+++ .github/common/resources/minikube-diagnostics.sh
@@ -15,6 +15,28 @@
 
 set -euo pipefail
 
+
+echo "=== 🧭 GatewayAPI debug ==="
+echo "=== 🧭 Gateways ==="
+kubectl get gateway -A
+kubectl get gateway -A -oyaml
+
+echo ""
+echo "=== 🧭 HTTPRoutes === "
+kubectl get httproutes -A
+kubectl get httproutes -A -oyaml
+
+echo ""
+echo "=== 🧭 ListenerSets ==="
+kubectl get listenersets -A
+kubectl get listenersets -A -oyaml
+
+echo ""
+echo "=== 🧭 Certificates ==="
+kubectl get certificates -A -oyaml
+kubectl get certificates -A -oyaml
+
+echo ""
 echo "=== 🧭 Cluster info ==="
 kubectl cluster-info || true
 kubectl get nodes -o wide || true
