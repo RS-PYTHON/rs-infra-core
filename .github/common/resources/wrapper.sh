d9d77cd3e7b57006d40b836ac87d3a62043379ac
nleconte-csgroup <nicolas.leconte@cs-soprasteria.com>
2026-08-05 16:19:50 +0200

    fix: remove patch of nginx and update patch of envoy

--- .github/common/resources/wrapper.sh
+++ .github/common/resources/wrapper.sh
@@ -174,7 +174,6 @@ run() {
 
 add_script ".github/common/resources/remove-apps.sh"
 add_script ".github/common/resources/patch-envoy.sh"
-add_script ".github/common/resources/patch-nginx.sh"
 
 ###############################################################################
 # Main
